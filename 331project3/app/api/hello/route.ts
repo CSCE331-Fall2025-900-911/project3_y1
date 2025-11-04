1b714ecdef7b3697bd2c0f719e287bba9b51b808
MrFuzzBall <martin00dan@gmail.com>
2025-11-03 18:47:08 -0600

    test

---
+++ 331project3/app/api/hello/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+
+  return NextResponse.json({ 
+    message: "Hello World from backend",
+    status: 200 
+  });
+}
